5507af4dac681e1a86d6aca425379dfa803fca41
Shane Kammerman <ShaneKammerman@shanes-macbook-pro.local>
2018-10-05 12:00:16 -0400

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Shane Kammerman on 10/5/18.
+//  Copyright © 2018 Shane Kammerman. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
